986604290e060832205ee8ac78b7f52f940818fc
jpetran2 <74488608+jpetran2@users.noreply.github.com>
2022-05-10 13:45:48 -0500

    resized button sizes

--- cell.py
+++ cell.py
@@ -9,6 +9,8 @@ class Cell:
     def create_btn_object(self, location):
         btn = Button(
             location,
+            width=12,
+            height=4,
             text = 'Text'
         )
         btn.bind('<Button-1>', self.left_click_actions)
